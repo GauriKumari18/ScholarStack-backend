bd1ee6b4336dfd6e38cd8bb7002581c53d8a4e8c
GauriKumari18 <gaurikumaribts@gmail.com>
2026-06-19 02:44:25 +0530

    implement PricingController with /calculate endpoint

---
+++ src/main/java/com/gaurikumari/marketplace/controller/PricingController.java
@@ -0,0 +1,27 @@
+package com.gaurikumari.marketplace.controller;
+
+import com.gaurikumari.marketplace.dto.Request.PricingRequest;
+import com.gaurikumari.marketplace.dto.Response.PricingResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.gaurikumari.marketplace.service.PricingService;
+
+@RestController
+@RequestMapping("/api/pricing")
+@RequiredArgsConstructor
+public class PricingController {
+
+    private final PricingService pricingService;
+
+    // POST /api/pricing/calculate
+    @PostMapping("/calculate")
+    public ResponseEntity<PricingResponse> calculatePricing(@RequestBody PricingRequest request) {
+        PricingResponse response = pricingService.calculatePricing(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
